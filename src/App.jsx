6e40cfc47835cc6935a17ff1261c3a2d461a26b1
Cliq India <cliqindia@Snehams-MacBook-Pro.local>
2025-10-17 12:50:01 +0530

    changes in Header

--- src/App.jsx
+++ src/App.jsx
@@ -301,7 +301,7 @@ function HomePage({ addToCart }) {
             <Button 
               size="lg" 
               variant="outline" 
-              className="border-white text-white hover:bg-white/10"
+              className="bg-white text-orange-600 hover:bg-gray-100"
               onClick={() => navigate('/donate')}
             >
               <Heart className="w-5 h-5 mr-2" />
